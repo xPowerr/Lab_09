8c532bedf17439f30587d978eaeeb2e852b99d33
xPowerr <125900506+xPowerr@users.noreply.github.com>
2023-04-21 09:16:07 -0600

    Creación de archivos
    Se crearon los archivos necesarios.

---
+++ Laboratorio_09.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Laboratorio_09.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Laboratorio_09.X.production.hex
